1455f81155222fb2029c29ee078211869e31d7dd
Gustavo Moro <gustavolego41@gmail.com>
2021-02-03 21:46:57 -0300

    BANANA

--- Program.cs
+++ Program.cs
@@ -14,12 +14,9 @@ namespace OrganizerParking
         [STAThread]
         static void Main()
         {
-            //SBASBABSABSAS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //Marhcaaaaaaaaa
-            //mamamasasa
         }
     }
 }
