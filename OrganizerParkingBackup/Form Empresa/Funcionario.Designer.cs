78f39eec94068bf9d6cfa347e48b00303c2ddd27
Gustavo Moro <78118591+Gustavolego41@users.noreply.github.com>
2021-02-13 18:53:06 -0300

    Add files via upload

---
+++ OrganizerParkingBackup/Form Empresa/Funcionario.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace OrganizerParking
+{
+    partial class Funcionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFuncionario = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFuncionario
+            // 
+            this.lblFuncionario.AutoSize = true;
+            this.lblFuncionario.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFuncionario.Location = new System.Drawing.Point(242, 36);
+            this.lblFuncionario.Name = "lblFuncionario";
+            this.lblFuncionario.Size = new System.Drawing.Size(187, 38);
+            this.lblFuncionario.TabIndex = 1;
+            this.lblFuncionario.Text = "Funcionário";
+            // 
+            // Funcionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 414);
+            this.Controls.Add(this.lblFuncionario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Funcionario";
+            this.Text = "Funcionario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFuncionario;
+    }
+}
