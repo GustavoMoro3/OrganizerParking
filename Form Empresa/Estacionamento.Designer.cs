51550d18120a24a64d8dc333d730873a7f1b5937
William <williamvazduggen@gmail.com>
2021-02-15 09:18:51 -0300

    William

--- Form Empresa/Estacionamento.Designer.cs
+++ Form Empresa/Estacionamento.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.lblEstacionamento = new System.Windows.Forms.Label();
+            this.lblVagas = new System.Windows.Forms.Label();
+            this.txtVagas = new System.Windows.Forms.TextBox();
+            this.btnVagas = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblEstacionamento
@@ -41,11 +44,42 @@
             this.lblEstacionamento.TabIndex = 1;
             this.lblEstacionamento.Text = "Estacionamento";
             // 
+            // lblVagas
+            // 
+            this.lblVagas.AutoSize = true;
+            this.lblVagas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVagas.Location = new System.Drawing.Point(60, 150);
+            this.lblVagas.Name = "lblVagas";
+            this.lblVagas.Size = new System.Drawing.Size(208, 25);
+            this.lblVagas.TabIndex = 2;
+            this.lblVagas.Text = "Quantidade de Vagas ";
+            // 
+            // txtVagas
+            // 
+            this.txtVagas.Location = new System.Drawing.Point(331, 150);
+            this.txtVagas.Name = "txtVagas";
+            this.txtVagas.Size = new System.Drawing.Size(161, 20);
+            this.txtVagas.TabIndex = 3;
+            // 
+            // btnVagas
+            // 
+            this.btnVagas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVagas.Location = new System.Drawing.Point(250, 251);
+            this.btnVagas.Name = "btnVagas";
+            this.btnVagas.Size = new System.Drawing.Size(170, 32);
+            this.btnVagas.TabIndex = 4;
+            this.btnVagas.Text = "Cadastrar";
+            this.btnVagas.UseVisualStyleBackColor = true;
+            this.btnVagas.Click += new System.EventHandler(this.btnVagas_Click);
+            // 
             // Estacionamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(677, 418);
+            this.Controls.Add(this.btnVagas);
+            this.Controls.Add(this.txtVagas);
+            this.Controls.Add(this.lblVagas);
             this.Controls.Add(this.lblEstacionamento);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Estacionamento";
@@ -58,5 +92,8 @@
         #endregion
 
         private System.Windows.Forms.Label lblEstacionamento;
+        private System.Windows.Forms.Label lblVagas;
+        private System.Windows.Forms.TextBox txtVagas;
+        private System.Windows.Forms.Button btnVagas;
     }
 }
