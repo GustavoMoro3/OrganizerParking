cfd19f6e3dea4a2249313735801b76e80ae19cf1
Gustavo <gustavolego41@gmail.com>
2021-02-15 09:55:41 -0300

    Perfil mostra

--- Form Funcionario/FormPerfil.Designer.cs
+++ Form Funcionario/FormPerfil.Designer.cs
@@ -82,7 +82,7 @@
             // 
             this.lblempresainformacao.AutoSize = true;
             this.lblempresainformacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblempresainformacao.Location = new System.Drawing.Point(305, 256);
+            this.lblempresainformacao.Location = new System.Drawing.Point(323, 256);
             this.lblempresainformacao.Name = "lblempresainformacao";
             this.lblempresainformacao.Size = new System.Drawing.Size(0, 31);
             this.lblempresainformacao.TabIndex = 5;
